7ce2330613ce8c69d71b73c6e28fd97f3173e5e1
ahuazhu <ahuazhu@gmail.com>
2017-04-01 10:25:46 +0800

    add akka

--- Soy-GateKeeper/src/main/java/com/ahuazhu/soy/executor/Executors.java
+++ Soy-GateKeeper/src/main/java/com/ahuazhu/soy/executor/Executors.java
@@ -1,9 +1,27 @@
 package com.ahuazhu.soy.executor;
 
+import akka.actor.AbstractActor;
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.actor.Props;
+import akka.japi.pf.ReceiveBuilder;
 import com.ahuazhu.soy.Soy;
 import com.ahuazhu.soy.modal.Query;
 import com.ahuazhu.soy.udp.ExecutorUtils;
+import com.ahuazhu.soy.utils.RecordBuilder;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.stereotype.Component;
+import org.xbill.DNS.Message;
+import org.xbill.DNS.Record;
+import org.xbill.DNS.Section;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.DatagramChannel;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.util.Iterator;
 import java.util.concurrent.ExecutorService;
 
 /**
@@ -27,9 +45,21 @@ public enum Executors implements Executor {
         }
     },
     Akka {
+        ActorSystem system = ActorSystem.create("actor-demo-java");
+        ActorRef soyActor = system.actorOf(Props.create(SoyActor.class));
+
         @Override
         public void execute(Query query) {
+            soyActor.tell(query, soyActor);
 
+        }
+
+        class SoyActor extends AbstractActor {
+
+            @Override
+            public Receive createReceive() {
+                return ReceiveBuilder.create().match(Query.class, Soy::fire).build();
+            }
         }
     },
 
