43a1e485e978f3f0222f106aba60bfdbc2ee2ce2
ahuazhu <ahuazhu@gmail.com>
2017-04-05 11:12:34 +0800

    add constants and configure interfae

---
+++ Soy-Core/src/main/java/com/ahuazhu/soy/config/Configure.java
@@ -0,0 +1,27 @@
+package com.ahuazhu.soy.config;
+
+import com.ahuazhu.soy.utils.Constants;
+
+/**
+ * Created by zhengwenzhu on 2017/4/5.
+ */
+public interface Configure {
+
+    default String getExecutorModal() {
+        return Constants.EXECUTE_MODE_SYNC;
+    }
+
+    default int getThreadPoolCoreSize() {
+        return 5;
+    }
+
+    default int getThreadPoolMaxSize() {
+        return Math.max(getThreadPoolCoreSize(), 10);
+    }
+
+    default int getThreadPoolQueueSize() {
+        return 10000;
+    }
+
+
+}
---
+++ Soy-Core/src/main/java/com/ahuazhu/soy/utils/Constants.java
@@ -0,0 +1,17 @@
+package com.ahuazhu.soy.utils;
+
+/**
+ * Created by zhengwenzhu on 2017/4/5.
+ */
+public class Constants {
+
+    public static final String EXECUTE_MODE_SYNC = "sync";
+
+    public static final String EXECUTE_MODE_THREAD_POOL = "threadpool";
+
+    public static final String EXECUTE_MODE_ACTOR = "actor";
+
+    public static final String EXECUTE_MODE_DISRUPTOR = "disruptor";
+
+
+}
